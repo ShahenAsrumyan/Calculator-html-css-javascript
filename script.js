9cc1cec6f8427e893a356a9115223229e5ad8e3b
Shahen <99030401+ShahenAsrumyan@users.noreply.github.com>
2022-03-19 01:21:08 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var buttonsCalc = document.querySelector(".buttonsss");
 var result = document.querySelector("#result");
-var razmo ;
+var history ;
 var answer1 ;
 buttonsCalc.addEventListener("click", function (event) {
     if (!event.target.classList.contains("calc_btn")) return;
@@ -14,17 +14,13 @@ buttonsCalc.addEventListener("click", function (event) {
             break;
         case "=":
             try {
-                 razmo = result.innerText;
+                 history = result.innerText;
                 result.innerText = eval(result.innerText);
                 result.value = eval(result.value);
                 answer1 = result.innerText;
-                // var arrray = [];
-                // arrray.push(result)
-                // console.log(arrray);
             } catch (err) {
                 alert("Enter the valid Input");
                 result.innerText = "";
-
             }
             break;
         case "+/-":
@@ -45,24 +41,19 @@ buttonsCalc.addEventListener("click", function (event) {
             myArray.pop();
             result.innerText = myArray.join("");
             break;
-
         case "√":
             value =result.innerText;
            var armat = Math.sqrt(Math.floor(value)).toFixed(5);
            result.innerText= armat;
             break;
-
-
-
         default:
             result.innerText += value;
-
     }
 });
 
 function clickHistory() {
     var array2 = [];
-    array2.push(razmo);
+    array2.push(history);
     var divis = document.createElement("div");  
   divis.setAttribute("style","width:320px;height:510px; border: 0.3px solid #C5C5C5; box-shadow: 0 0 13px rgba(0, 0, 0, 0.4); background-color:white;  border-radius: 9px;color: black; position: fixed;"); 
   var parag = document.createElement("p"); 
